5e55f3d7386295456942bc9f863c776fed34d500
puurrii <sabinpuri06@gmail.com>
2023-11-18 12:34:05 +0545

    added hamburger menu

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.querySelector('.nav-toogle');
-    const navClose = document.querySelector('.nav-close');
-    const navMenu = document.querySelector('.nav-menu');
+const navToogle = document.querySelector('.nav-toogle')
+const navClose = document.querySelector('.nav-close')
+const navMenu = document.querySelector('.helo')
 
-    navToggle.addEventListener('click', function () {
-        navMenu.classList.add('show-menu');
-        navToggle.style.display = 'none';
-        navClose.style.display = 'block';
-    });
-
-    navClose.addEventListener('click', function () {
-        navMenu.classList.remove('show-menu');
-        navToggle.style.display = 'block';
-        navClose.style.display = 'none';
-    });
-});
+navToogle.addEventListener('click', function() {
+    navMenu.style.display = "block";
+    navToogle.style.display = "none";
+    navClose.style.display = "block"
+})
+navClose.addEventListener('click', function() {
+    navMenu.style.display = "none";
+    navToogle.style.display = "block";
+    navClose.style.display = "none"
+})
