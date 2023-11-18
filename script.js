0dc31ecfce0595750c501d6d007420e7bb232770
puurrii <sabinpuri06@gmail.com>
2023-11-18 08:53:19 +0545

    navbar for phone

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     navToggle.addEventListener('click', function () {
         navMenu.classList.add('show-menu');
-        navToggle.classList.add('hide-icon');
-        navClose.classList.remove('hide-icon');
     });
 
     navClose.addEventListener('click', function () {
         navMenu.classList.remove('show-menu');
-        navToggle.classList.remove('hide-icon');
-        navClose.classList.add('hide-icon');
     });
 });
