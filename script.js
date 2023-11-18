41261fba8a6f65507ba44619b1b05b1e29531497
puurrii <sabinpuri06@gmail.com>
2023-11-18 13:12:35 +0545

    idk

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const navToogle = document.querySelector('.nav-toogle')
-const navClose = document.querySelector('.nav-close')
-const navMenu = document.querySelector('.helo')
+const navToggle = document.querySelector('.nav-toogle');
+const navClose = document.querySelector('.nav-close');
+const navMenu = document.querySelector('.helo');
 
-navToogle.addEventListener('click', function() {
-    navMenu.style.display = "block";
-    navToogle.style.display = "none";
-    navClose.style.display = "block"
-})
+navToggle.addEventListener('click', function() {
+    navMenu.style.display = 'block';
+    navToggle.style.display = 'none';
+    navClose.style.display = 'block';
+});
+
 navClose.addEventListener('click', function() {
-    navMenu.style.display = "none";
-    navToogle.style.display = "block";
-    navClose.style.display = "none"
-})
+    navMenu.style.display = 'none';
+    navToggle.style.display = 'block';
+    navClose.style.display = 'none';
+});
 
-const navLink = document.querySelectorAll('.nav-item')
- navLink.addEventListener('çlick', function(){
-    navMenu.style.display = "none"
-    navClose.style.display = "none"
-    navToogle.style.display = "block"
- })
+const navLinks = document.querySelectorAll('.nav-item');
+
+navLinks.forEach(function(navLink) {
+    navLink.addEventListener('click', function() {
+        navMenu.style.display = 'none';
+        navClose.style.display = 'none';
+        navToggle.style.display = 'block';
+    });
+});
