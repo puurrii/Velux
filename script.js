8627ce9d3822d4f55165fbc9986a9c499e4250c3
puurrii <sabinpuri06@gmail.com>
2023-11-21 17:05:54 +0545

    completed navbar

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
-const isMobile = window.innerWidth <= 600; // Adjust the threshold based on your needs
-
-if (isMobile) {
-    const navToggle = document.querySelector('.nav-toogle');
-    const navClose = document.querySelector('.nav-close');
-    const navMenu = document.querySelector('.helo');
-
-    navToggle.addEventListener('click', function() {
-        navMenu.style.display = 'block';
-        navToggle.style.display = 'none';
-        navClose.style.display = 'block';
-    });
-
-    navClose.addEventListener('click', function() {
-        navMenu.style.display = 'none';
-        navToggle.style.display = 'block';
-        navClose.style.display = 'none';
-    });
-
-    const navLinks = document.querySelectorAll('.nav-item');
-
-    navLinks.forEach(function(navLink) {
-        navLink.addEventListener('click', function() {
-            navMenu.style.display = 'none';
-            navClose.style.display = 'none';
-            navToggle.style.display = 'block';
-        });
-    });
-    const navSocialLinks = document.querySelectorAll('.social-links');
-
-    navSocialLinks.forEach(function(navSocialLink) {
-        navSocialLink.addEventListener('click', function() {
-            navMenu.style.display = 'none';
-            navClose.style.display = 'none';
-            navToggle.style.display = 'block';
-        });
-    });    
+const isMobile = window.innerWidth <= 600;
+if(isMobile){
+    const navtoggle = document.querySelector('.nav-toogle')
+    const navclose = document.querySelector('.nav-close')
+    const navmenu = document.querySelector('.nav-menus')
+    
+   navtoggle.addEventListener('click', function () {
+         navtoggle.style.display = 'none'
+         navclose.style.display = 'block'
+         navmenu.style.display = 'block'
+   })
+   navclose.addEventListener('click', () => {
+    navclose.style.display = 'none'
+    navmenu.style.display = 'none'
+    navtoggle.style.display = 'block'
+   })
+   navmenu.addEventListener('click', () => {
+    navclose.style.display = 'none'
+    navmenu.style.display = 'none'
+    navtoggle.style.display = 'block'
+   })
 }
