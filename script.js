caebdd246756cd30306d32c47b75d40f7742bf0b
puurrii <sabinpuri06@gmail.com>
2023-12-03 15:50:00 +0545

    ffixed bugs

--- script.js
+++ script.js
@@ -19,30 +19,22 @@ if(isMobile){
     navmenu.style.display = 'none'
     navtoggle.style.display = 'block'
    })
+
+
+}
+    const images = document.querySelectorAll(".pimg img")
+    const imageSlider = document.querySelector("#imageSlider")
+    const totalimages = images.length
+    let currentIndex = 0;
+    function showNextImage() {
+        images[currentIndex].style.display = 'none'
+        currentIndex = (currentIndex + 1) % totalimages;
+        images[currentIndex].style.display = 'block'
     }
-    //need to study
-    document.addEventListener("DOMContentLoaded", function () {
-      // Get references to the images and container
-      const images = document.querySelectorAll(".pimg img");
-      const imageContainer = document.getElementById("imageSlider");
+    
+    function startImageSlider() {
+        setInterval(showNextImage, 4000)
+    }
   
-      // Set initial image index and total number of images
-      let currentIndex = 0;
-      const totalImages = images.length;
-  
-      // Function to show the next image
-      function showNextImage() {
-          images[currentIndex].style.display = "none";
-          currentIndex = (currentIndex + 1) % totalImages;
-          images[currentIndex].style.display = "block";
-      }
-  
-      // Function to start the image slider
-      function startImageSlider() {
-          setInterval(showNextImage, 5000); // Change the duration as needed (in milliseconds)
-      }
-  
-      // Start the image slider when the DOM is loaded
-      startImageSlider();
-  });
-  
+startImageSlider();
+
